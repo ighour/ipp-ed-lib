86fa54143b9b83987fd345907951f108c3cdf6ed
ighour <ti.celio.rodrigues@gmail.com>
2019-01-20 11:44:23 +0000

    Refactored to use BaseQueueADT for Queue and PriorityQueue.

--- estg/ed/interfaces/PriorityQueueADT.java
+++ estg/ed/interfaces/PriorityQueueADT.java
@@ -5,15 +5,13 @@
  */
 package estg.ed.interfaces;
 
-import estg.ed.exceptions.EmptyCollectionException;
-
 /**
  * Contract for Priority Queues.
  * Uses double type for priority.
  * @author igu
  * @param <T>
  */
-public interface PriorityQueueADT<T> {
+public interface PriorityQueueADT<T> extends BaseQueueADT<T> {
   /** 
    * Adds one element to this priority queue.
    * Uses double type for priority.
@@ -21,37 +19,4 @@ public interface PriorityQueueADT<T> {
    * @param priority
    */
    public void enqueue(T element, double priority);
-   
-  /** 
-   * Removes and returns the element at the front of this queue.
-   * @return the element at the front of this queue
-   * @throws estg.ed.exceptions.EmptyCollectionException
-   */
-   public T dequeue() throws EmptyCollectionException;
-   
-  /** 
-   * Returns without removing the element at the front of this queue.
-   * @return the first element in this queue
-   * @throws estg.ed.exceptions.EmptyCollectionException
-   */
-   public T first() throws EmptyCollectionException;
-   
-  /** 
-   * Returns true if this queue contains no elements.
-   * @return boolean whether or not this queue is empty
-   */
-   public boolean isEmpty();
-   
-  /** 
-   * Returns the number of elements in this queue.
-   * @return int number of elements in this queue
-   */
-   public int size();
-   
-  /** 
-   * Returns a string representation of this queue.
-   * @return String representation of this queue
-   */
-   @Override
-   public String toString();
 }
--- estg/ed/interfaces/QueueADT.java
+++ estg/ed/interfaces/QueueADT.java
@@ -5,50 +5,15 @@
  */
 package estg.ed.interfaces;
 
-import estg.ed.exceptions.EmptyCollectionException;
-
 /**
- * Contract for Queues
+ * Contract for Queues.
  * @author igu
  * @param <T>
  */
-public interface QueueADT<T> {
+public interface QueueADT<T> extends BaseQueueADT<T> {
   /** 
    * Adds one element to the rear of this queue.
    * @param element the element to be added to the rear of this queue
    */
    public void enqueue(T element);
-   
-  /** 
-   * Removes and returns the element at the front of this queue.
-   * @return the element at the front of this queue
-   * @throws estg.ed.exceptions.EmptyCollectionException
-   */
-   public T dequeue() throws EmptyCollectionException;
-   
-  /** 
-   * Returns without removing the element at the front of this queue.
-   * @return the first element in this queue
-   * @throws estg.ed.exceptions.EmptyCollectionException
-   */
-   public T first() throws EmptyCollectionException;
-   
-  /** 
-   * Returns true if this queue contains no elements.
-   * @return boolean whether or not this queue is empty
-   */
-   public boolean isEmpty();
-   
-  /** 
-   * Returns the number of elements in this queue.
-   * @return int number of elements in this queue
-   */
-   public int size();
-   
-  /** 
-   * Returns a string representation of this queue.
-   * @return String representation of this queue
-   */
-   @Override
-   public String toString();
 }
